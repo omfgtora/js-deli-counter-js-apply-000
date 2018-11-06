254b7f4ff483b95dfb6abd5935ed767445551f15
reflective-mapping-9478 <ejrobe@gmail.com>
2018-11-06 22:36:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,12 +17,20 @@ function nowServing(katzDeliLine){
 function currentLine(katzDeliLine){
   var output = "The line is currently: ";
   
+<<<<<<< HEAD
   for (var i = 0; i < katzDeliLine.length; i++){
     var place = i + 1
     if (i != katzDeliLine.length - 1){
       output = output + place + ". " + katzDeliLine[i] + ", "
     } else {
       output = output + place + ". " + katzDeliLine[i]
+=======
+  for (var i = katzDeliLine.length; i < 0; i--){
+    if (i > 0){
+      output = output + i + ". " + katzDeliLine[i] + ", "
+    } else {
+      output = output + i + ". " + katzDeliLine[i]
+>>>>>>> e5470cbea1173c948f5d6b730f19fd3957e332b0
     }
   }
   
@@ -33,4 +41,15 @@ function currentLine(katzDeliLine){
     delete output[len];
     return output
   }
+}
+
+var customerCount = 0;
+
+function takeANumberAlt(katzDeliLine){
+  // Add to counter (increment)
+  customerCount++;
+  // add new number to array
+  katzDeliLine.push(count);
+  // exit
+  return count
 }
